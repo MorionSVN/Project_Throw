0dd3a13577b99180b399a15bd3b0eb8ae9075216
ReverendGates <ilja.epic1337@gmail.com>
2024-05-31 23:26:59 +0300

    Final commit

--- src/reg.php
+++ src/reg.php
@@ -1,19 +1,24 @@
 <?php
+header('Content-Type: application/json');
+
 $db = new mysqli('localhost', 'reverendo_throw', 'Synyster7', 'reverendo_throw');
 
 if ($db->connect_error) {
-    die('Ошибка соединения: ' . $db->connect_error);
+    die(json_encode(['error' => 'Ошибка соединения: ' . $db->connect_error]));
 }
 
-$mail = $db->real_escape_string($_POST['Mail']);
-$password = $db->real_escape_string($_POST['Password']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $mail = $db->real_escape_string($_POST['Mail']);
+    $password = $db->real_escape_string($_POST['Password']);
 
-$query = "INSERT INTO USERS (`Mail`, `Password`) VALUES ('$mail', '$password')";
+    $query = "INSERT INTO USERS (`Mail`, `Password`) VALUES ('$mail', '$password')";
 
-if ($db->query($query)) {
-    header('Location: ../index.php');
-} else {
-    echo "Ошибка: " . $db->error;
+    if ($db->query($query)) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['error' => 'Ошибка: ' . $db->error]);
+    }
 }
 
 $db->close();
+?>
